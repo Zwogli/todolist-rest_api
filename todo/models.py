5acd8f07d2eedb3849e369688d7f9b7d3a9e0019
Mathias Kohler <123494012+Zwogli@users.noreply.github.com>
2024-03-12 23:07:17 +0100

    Add new model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,15 @@
+import datetime
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+    created_at = models.DateField(
+      default=datetime.date.today
+      )
+    user = models.ForeignKey(                   #ForeignKey verweißt auf ein anderes Model
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
